89d2686a491eee5c516dcb9b9addeebe07fa89d7
Adam Brezina <breziinaadam@seznam.cz>
2026-04-19 10:42:40 +0200

    Created persona structure and dummy persona yaml file

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,2 +1,34 @@
 import * as z from 'zod'
 
+export const PersonalDetailsSchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  github: z.string().url().optional(),
+  linkedin: z.string().url().optional(),
+  portfolioPage: z.string().url().optional()
+})
+
+export const WorkExperienceSchema = z.object({
+  companyName: z.string(),
+  date: z.string(),
+  role: z.string(),
+  description: z.array(z.string()).optional()
+})
+
+export const EducationSchema = z.object({
+  school: z.string(),
+  date: z.string(),
+  degree: z.string()
+})
+
+export const SkillsSchema = z.array(z.string())
+
+export const PersonaSchema = z.object({
+  personal: PersonalDetailsSchema,
+  work: z.array(WorkExperienceSchema),
+  education: z.array(EducationSchema),
+  skills: SkillsSchema
+})
+
+export type Persona = z.infer<typeof PersonaSchema>;
+
